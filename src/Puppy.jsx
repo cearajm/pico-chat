48ef5dcf12abc4fc6daad15bc9105074b22063ea
ceara <cearamahinay@gmail.com>
2026-09-19 03:11:14 -0600

    feat: say hello...

---
+++ src/Puppy.jsx
@@ -0,0 +1,8 @@
+
+// hellooooo
+
+function Puppy() {
+    return <h1>hello and welcome to pico chat !!</h1>;
+}
+
+export default Puppy;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Puppy from './Puppy.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Puppy />
+  </StrictMode>,
+)
